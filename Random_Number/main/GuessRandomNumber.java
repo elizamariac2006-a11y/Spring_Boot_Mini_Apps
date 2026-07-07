396ac92a200306e69a5afc1dd64035f938c69caf
Eliza-Maria Ciuc <eliza.maria.c.2006@gmail.com>
2026-07-07 15:29:24 +0300

    Create GuessRandomNumber.java

---
+++ Random_Number/main/GuessRandomNumber.java
@@ -0,0 +1,51 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class GuessRandomNumber {
+    private int secret_number = 0;
+    private boolean game_is_active = false;
+    private int chances = 0;
+
+    @GetMapping("/start")
+    String beggining_message() {
+        return "Welcome! <br> " +
+                "Guess the natural number in the range [1, 100]. <br> " +
+                "Select the mode by writing http://localhost:8080/mode?mode=... " +
+                "and replace .. with a difficulty level(hard, easy, medium). <br>" +
+                "Then you will have 15(easy), 10(medium) or 5(hard) chances. <br>" +
+                "In order to take a guess, write http://localhost:8080/guess?guess=... and replace" +
+                " ... with a number(your guess). <br>" +
+                "In order to verify if a game is already started, go to /active. <br>";
+    }
+
+
+    @GetMapping("/mode")
+    String get_mode(@RequestParam String mode) {
+        Random random = new Random();
+        secret_number = random.nextInt() % 100 + 1;
+        if(mode.equalsIgnoreCase(("easy"))) {
+            game_is_active = true;
+            chances = 15;
+            return "Game started. \n Difficulty : easy. \n You have 15 chances.";
+        }
+        if(mode.equalsIgnoreCase("medium")) {
+            game_is_active = true;
+            chances = 10;
+            return "Game started. \n Difficulty : medium. \n You have 10 chances.";
+        }
+        if(mode.equalsIgnoreCase("hard" )) {
+            game_is_active = true;
+            chances = 10;
+            return "Game started. \n Difficulty : hard. \n You have 5 chances.";
+        }
+        game_is_active = false;
+        chances = 0;
+        return "Difficulty level not valid!";
+    }
+}
